7fdd5770a17f408105e554040fbe558622150edb
Melvin Franzen <melvin.franzen.eon@gmail.com>
2026-03-15 19:05:15 +0000

    File changes

--- src/pages/B2BOrders.jsx
+++ src/pages/B2BOrders.jsx
@@ -36,7 +36,8 @@ export default function B2BOrders() {
       Promise.all([
         base44.entities.PrintOrder.filter({ created_by: u.email }, "-created_date", 100),
         base44.entities.Case.filter({ created_by: u.email }, "-created_date", 100),
-      ]).then(([o, c]) => { setOrders(o); setCases(c); setLoading(false); });
+        base44.entities.FuneralHome.filter({ created_by: u.email }, "-created_date", 1),
+      ]).then(([o, c, fh]) => { setOrders(o); setCases(c); setFuneralHome(fh[0] || null); setLoading(false); });
     });
   }, []);
 
